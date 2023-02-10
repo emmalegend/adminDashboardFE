5cca0910d1fe09569e5a25f69cf53922f40a65d6
Emmanuel Effiong <emmanuel.effiong361@gmail.com>
2023-02-10 12:41:30 +0100

    worked on the product page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
-import Layout from "screens/layout";
-import Dashboard from "screens/dashboard";
+import Layout from "screens/Layout";
+import Dashboard from "screens/Dashboard";
+import Products from "screens/Products";
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -18,6 +19,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
